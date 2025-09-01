4722a013357ee270591b37a81ac5d7ffc2b7609c
Peter Tran Quoc Phong <phongtq@sna.edu.vn>
2025-09-01 14:47:10 +0700

    Remove unused imports in tests

--- tests/factories.py
+++ tests/factories.py
@@ -1,9 +1,11 @@
 import factory
 
+
 class ProductFactory(factory.Factory):
     class Meta:
         model = dict
-    id = factory.Sequence(lambda n: n+1)
+
+    id = factory.Sequence(lambda n: n + 1)
     name = factory.Faker("word")
     category = factory.Iterator(["toys", "electronics", "books"])
     available = factory.Iterator([True, False])
